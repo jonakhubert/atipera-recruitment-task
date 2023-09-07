3b46f06c8e2750ca2ffe55a74135b26c887ac86c
jonakhubert <75631040+jonakhubert@users.noreply.github.com>
2023-09-07 21:20:50 +0200

    Initial commit

---
+++ src/main/java/com/atipera/repoviewer/RepoviewerApplication.java
@@ -0,0 +1,13 @@
+package com.atipera.repoviewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepoviewerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepoviewerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atipera/repoviewer/RepoviewerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atipera.repoviewer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepoviewerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
