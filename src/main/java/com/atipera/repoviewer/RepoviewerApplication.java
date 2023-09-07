89283308397aca50e4cca12fd280b35f8d7fc6b5
jonakhubert <75631040+jonakhubert@users.noreply.github.com>
2023-09-07 23:27:56 +0200

    Set up the application

--- src/main/java/com/atipera/repoviewer/RepoviewerApplication.java
+++ src/main/java/com/atipera/repoviewer/RepoviewerApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RepoviewerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(RepoviewerApplication.class, args);
 	}
-
 }
