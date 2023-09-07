ef92913ac14ead11511314a6c662b8e27138e5d7
jonakhubert <75631040+jonakhubert@users.noreply.github.com>
2023-09-07 23:27:45 +0200

    Add repo service

---
+++ src/main/java/com/atipera/repoviewer/service/RepoService.java
@@ -0,0 +1,23 @@
+package com.atipera.repoviewer.service;
+
+import com.atipera.repoviewer.model.RepoResponse;
+import com.atipera.repoviewer.repository.IRepoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class RepoService {
+
+    private final IRepoRepository repoRepository;
+
+    @Autowired
+    public RepoService(IRepoRepository repoRepository) {
+        this.repoRepository = repoRepository;
+    }
+
+    public List<RepoResponse> getUserRepositories(String username) {
+        return repoRepository.getRepositoriesByUsername(username);
+    }
+}
