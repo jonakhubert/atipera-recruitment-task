4f7e5b9dca5a73989147374fdb32f81f5aac0a97
jonakhubert <75631040+jonakhubert@users.noreply.github.com>
2023-09-08 18:51:40 +0200

    Handle user not found and media type not supported exceptions

--- src/main/java/com/atipera/repoviewer/exception/GlobalExceptionHandler.java
+++ src/main/java/com/atipera/repoviewer/exception/GlobalExceptionHandler.java
@@ -2,45 +2,44 @@ package com.atipera.repoviewer.exception;
 
 import com.atipera.repoviewer.exception.model.ApiError;
 import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.http.*;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.HttpMediaTypeNotAcceptableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 
 @ControllerAdvice
-public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+public class GlobalExceptionHandler {
 
-    @Override
-    protected ResponseEntity<Object> handleHttpMediaTypeNotAcceptable(
+    @ExceptionHandler(HttpClientErrorException.class)
+    public ResponseEntity<ApiError> handleUserNotFoundException(
+        HttpClientErrorException ex,
+        HttpServletRequest request
+    ) {
+        ApiError apiError = new ApiError(
+            HttpStatus.NOT_FOUND.value(),
+            "User not found."
+        );
+
+        return new ResponseEntity<>(apiError, setHeaders(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(HttpMediaTypeNotAcceptableException.class)
+    public ResponseEntity<ApiError> handleMediaTypeNotAcceptableException(
         HttpMediaTypeNotAcceptableException ex,
-        HttpHeaders headers,
-        HttpStatusCode status,
-        WebRequest request
+        HttpServletRequest request
     ) {
         ApiError apiError = new ApiError(
             HttpStatus.NOT_ACCEPTABLE.value(),
-            ex.getMessage()
+            request.getHeader("Accept") + " not acceptable."
         );
 
-        return handleExceptionInternal(ex, apiError, setHeaders(), HttpStatus.NOT_ACCEPTABLE, request);
+        return new ResponseEntity<>(apiError, setHeaders(), HttpStatus.NOT_ACCEPTABLE);
     }
-
-//    @ExceptionHandler(HttpClientErrorException.NotFound.class)
-//    public ResponseEntity<ApiError> handleUserNotFoundException(
-//        HttpClientErrorException.NotFound ex,
-//        HttpServletRequest request
-//    ) {
-//        ApiError apiError = new ApiError(
-//            HttpStatus.NOT_FOUND.value(),
-//            ex.getMessage()
-//        );
-//
-//        return new ResponseEntity<>(apiError, HttpStatus.NOT_FOUND);
-//    }
 
     private HttpHeaders setHeaders() {
         HttpHeaders headers = new HttpHeaders();
