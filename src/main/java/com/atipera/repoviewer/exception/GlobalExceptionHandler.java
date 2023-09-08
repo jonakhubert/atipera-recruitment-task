91c337474af226450d58f77d29ac26001496eee5
jonakhubert <75631040+jonakhubert@users.noreply.github.com>
2023-09-08 18:50:14 +0200

    Add unit tests

--- src/main/java/com/atipera/repoviewer/exception/GlobalExceptionHandler.java
+++ src/main/java/com/atipera/repoviewer/exception/GlobalExceptionHandler.java
@@ -2,46 +2,50 @@ package com.atipera.repoviewer.exception;
 
 import com.atipera.repoviewer.exception.model.ApiError;
 import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.HttpMediaTypeNotAcceptableException;
-import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import java.time.LocalDateTime;
 
 @ControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler(HttpClientErrorException.NotFound.class)
-    public ResponseEntity<ApiError> handleUserNotFoundException(
-        HttpClientErrorException.NotFound ex,
-        HttpServletRequest request
+    @Override
+    protected ResponseEntity<Object> handleHttpMediaTypeNotAcceptable(
+        HttpMediaTypeNotAcceptableException ex,
+        HttpHeaders headers,
+        HttpStatusCode status,
+        WebRequest request
     ) {
         ApiError apiError = new ApiError(
-            request.getRequestURI(),
-            ex.getMessage(),
-            HttpStatus.NOT_FOUND.value(),
-            LocalDateTime.now().toString()
+            HttpStatus.NOT_ACCEPTABLE.value(),
+            ex.getMessage()
         );
 
-        return new ResponseEntity<>(apiError, HttpStatus.NOT_FOUND);
+        return handleExceptionInternal(ex, apiError, setHeaders(), HttpStatus.NOT_ACCEPTABLE, request);
     }
 
-    @ExceptionHandler(HttpMediaTypeNotAcceptableException.class)
-    public ResponseEntity<ApiError> handleUnsupportedMediaTypeException(
-        HttpMediaTypeNotAcceptableException ex,
-        HttpServletRequest request
-    ) {
-        ApiError apiError = new ApiError(
-            request.getRequestURI(),
-            ex.getMessage(),
-            HttpStatus.NOT_ACCEPTABLE.value(),
-            LocalDateTime.now().toString()
-        );
+//    @ExceptionHandler(HttpClientErrorException.NotFound.class)
+//    public ResponseEntity<ApiError> handleUserNotFoundException(
+//        HttpClientErrorException.NotFound ex,
+//        HttpServletRequest request
+//    ) {
+//        ApiError apiError = new ApiError(
+//            HttpStatus.NOT_FOUND.value(),
+//            ex.getMessage()
+//        );
+//
+//        return new ResponseEntity<>(apiError, HttpStatus.NOT_FOUND);
+//    }
 
-        return new ResponseEntity<>(apiError, HttpStatus.NOT_ACCEPTABLE);
+    private HttpHeaders setHeaders() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        return headers;
     }
 }
