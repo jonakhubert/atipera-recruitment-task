c90df82090044709ea698f0d60ce1817acd6eb7f
jonakhubert <75631040+jonakhubert@users.noreply.github.com>
2023-09-08 18:52:07 +0200

    Remove redundant fields

--- src/main/java/com/atipera/repoviewer/exception/model/ApiError.java
+++ src/main/java/com/atipera/repoviewer/exception/model/ApiError.java
@@ -1,8 +1,6 @@
 package com.atipera.repoviewer.exception.model;
 
 public record ApiError(
-        String path,
-        String message,
         int statusCode,
-        String timestamp
+        String message
 ) {}
